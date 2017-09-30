05168a9a8a9c0f8eb14f2184ed42544e04822159
Deokjae <deokjj@gmail.com>
2017-09-30 14:27:16 -0400

    deokjae stuff

---
+++ watchoutBackend/config.js
@@ -0,0 +1,5 @@
+export const google = {
+  clientID: '520753576201-18cuu98674ei042ostofg228isa9tjts.apps.googleusercontent.com',
+  clientSecret: 'oxUIf16hmIVakGCfGFOUTGfo',
+  callbackURL: 'http://localhost:3000/auth/google/callback',
+};
---
+++ watchoutCli/app.js
@@ -0,0 +1,157 @@
+import React, { Component } from 'react';
+import {
+  Image,
+  Linking,
+  StyleSheet,
+  Platform,
+  Text,
+  View
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import SafariView from 'react-native-safari-view';
+
+export default class App extends Component {
+
+  state = {
+    user: undefined, // user has not logged in yet
+  };
+
+  // Set up Linking
+  componentDidMount() {
+    // Add event listener to handle OAuthLogin:// URLs
+    Linking.addEventListener('url', this.handleOpenURL);
+    // Launched from an external URL
+    Linking.getInitialURL().then((url) => {
+      if (url) {
+        this.handleOpenURL({ url });
+      }
+    });
+  };
+
+  componentWillUnmount() {
+    // Remove event listener
+    Linking.removeEventListener('url', this.handleOpenURL);
+  };
+
+  handleOpenURL = ({ url }) => {
+    // Extract stringified user string out of the URL
+    const [, user_string] = url.match(/user=([^#]+)/);
+    this.setState({
+      // Decode the user string and parse it into JSON
+      user: JSON.parse(decodeURI(user_string))
+    });
+    if (Platform.OS === 'ios') {
+      SafariView.dismiss();
+    }
+  };
+
+  // Handle Login with Facebook button tap
+  // loginWithFacebook = () => this.openURL('http://localhost:3000/auth/facebook');
+
+  // Handle Login with Google button tap
+  loginWithGoogle = () => this.openURL('http://localhost:3000/auth/google');
+
+  // Open URL in a browser
+  openURL = (url) => {
+    // Use SafariView on iOS
+    if (Platform.OS === 'ios') {
+      SafariView.show({
+        url: url,
+        fromBottom: true,
+      });
+    }
+    // Or Linking.openURL on Android
+    else {
+      Linking.openURL(url);
+    }
+  };
+
+  render() {
+    const { user } = this.state;
+    return (
+      <View style={styles.container}>
+        { user
+          ? // Show user info if already logged in
+            <View style={styles.content}>
+              <Text style={styles.header}>
+                Welcome {user.name}!
+              </Text>
+              <View style={styles.avatar}>
+                <Image source={{ uri: user.avatar }} style={styles.avatarImage} />
+              </View>
+            </View>
+          : // Show Please log in message if not
+            <View style={styles.content}>
+            <Image source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/COPWATCH_logo.svg/1200px-COPWATCH_logo.svg.png'}}
+             style={styles.copwatchLogo} />
+              <Text style={styles.text}>
+                Welcome to WatchOut👮🏻👮🏽‍♀️ {'\n'}
+                Please log in to continue {'\n'}
+                to the awesomness
+              </Text>
+            </View>
+        }
+        {/* Login buttons */}
+        <View style={styles.buttons}>
+          <Icon.Button
+            name="google"
+            backgroundColor="#DD4B39"
+            onPress={this.loginWithGoogle}
+            {...iconStyles}
+            style={{alignItems: 'center'}}
+          >
+            Or with Google
+          </Icon.Button>
+          <Icon.Button
+          backgroundColor="white"
+          ></Icon.Button>
+        </View>
+      </View>
+    );
+  }
+}
+
+const iconStyles = {
+  borderRadius: 10,
+  iconStyle: { paddingVertical: 5 },
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: 20,
+  },
+  copwatchLogo: {
+    width: 250,
+    height: 170
+  },
+  avatarImage: {
+    borderRadius: 50,
+    height: 100,
+    width: 100,
+  },
+  header: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  text: {
+    textAlign: 'center',
+    color: '#333',
+    marginBottom: 5,
+  },
+  buttons: {
+    justifyContent: 'space-between',
+    flexDirection: 'column',
+    margin: 20,
+    marginBottom: 30,
+  },
+});
